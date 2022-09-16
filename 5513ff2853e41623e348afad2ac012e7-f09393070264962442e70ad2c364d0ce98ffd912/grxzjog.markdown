7d75cee04143867b4385dfd9f64c2179f286df8d
PatPaterra <113700401+PatPaterra@users.noreply.github.com>
2022-09-16 10:58:14 -0300

    Add files via upload

---
+++ 5513ff2853e41623e348afad2ac012e7-f09393070264962442e70ad2c364d0ce98ffd912/grxzjog.markdown
@@ -0,0 +1,7 @@
+GRxzjOG
+-------
+
+
+A [Pen](https://codepen.io/CebolaErto/pen/GRxzjOG) by [Felipe](https://codepen.io/CebolaErto) on [CodePen](https://codepen.io).
+
+[License](https://codepen.io/license/pen/GRxzjOG).
---
+++ 5513ff2853e41623e348afad2ac012e7-f09393070264962442e70ad2c364d0ce98ffd912/script.js
@@ -0,0 +1,18 @@
+function bodyload()
+{ document.getElementById("bt").style.fontSize="15px";
+document.getElementById("bt").style.backgroundColor="blue";
+document.getElementById("bt").style.borderRadius="20px";
+document.getElementById("bt").style.color="white";
+document.getElementById("bt").style.padding="10px";
+document.getElementById("bt").style.borderStyle="none";}
+
+function mudaraimagem(){
+  document.getElementById("imagem_desenho").src="http://3.bp.blogspot.com/-3YJf5BUcJhs/ViqBfOdVbfI/AAAAAAAA0cM/dm5SouAhCxU/s1600/Tio-Patinhas%2BE%2Bfamilia%2B%252814%2529.png";
+}
+function voltarimagem(){
+  document.getElementById("imagem_desenho").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoMAHBY3m9EHjwa7nlDq9xy5IdqyqqN3YwFA&usqp=CAU" ;
+}
+function changetheme(){
+  document.getElementById("body").style.backgroundColor="yellow";
+  document.getElementById("nomedotítulo").style.backgroundColor="red"
+}
